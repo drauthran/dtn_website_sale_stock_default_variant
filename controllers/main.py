165e24e808eb730e42deaa54e088b5ce4c86b9dd
drauthran <drauthran@gmail.com>
2025-10-01 19:55:00 +0300

    feat(website_sale): Автовыбор первого варианта в наличии
    Добавлена логика для автоматической активации первого доступного для покупки варианта товара при загрузке страницы. Если все варианты отсутствуют, остаётся выбранным первый по умолчанию.

--- controllers/main.py
+++ controllers/main.py
@@ -7,13 +7,22 @@ class DtnWebsiteSale(WebsiteSale):
     @http.route()
     def product(self, product, category='', search='', **kwargs):
         res = super(DtnWebsiteSale, self).product(product, category, search, **kwargs)
+
         if len(product.attribute_line_ids) != 1:
             return res
+
         stock_map = {}
-        for variant in product.product_variant_ids:
+        # --- ИЗМЕНЕНИЕ ЗДЕСЬ ---
+        # Мы получаем варианты с правами суперпользователя, чтобы обойти проверку прав доступа для гостей
+        variants_with_sudo = product.product_variant_ids.sudo()
+        
+        # Теперь итерируем по вариантам, полученным через .sudo()
+        for variant in variants_with_sudo:
             if len(variant.product_template_attribute_value_ids) == 1:
                 ptav_id = variant.product_template_attribute_value_ids.id
+                # Теперь это поле будет вычислено без ошибок доступа
                 is_in_stock = variant.virtual_available > 0
                 stock_map[ptav_id] = is_in_stock
+        
         res.qcontext['dtn_stock_map_json'] = json.dumps(stock_map)
         return res
