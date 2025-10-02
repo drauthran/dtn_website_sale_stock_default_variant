df20dd80b6a51116a1993efa2a20569c3704c239
drauthran <drauthran@gmail.com>
2025-10-02 12:16:40 +0300

    готовый модуль с хардкодным "Немає на складі"

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'DTN Website Sale: Stock Variants Style',
-    'version': '18.0.5.0.0', # Финальная версия
+    'version': '18.0.9.0.0', # Финальная версия
     'category': 'Website/eCommerce',
-    'summary': 'Visually marks out-of-stock product variants on the product page.',
+    'summary': 'Visually marks out-of-stock variants and auto-selects the first available one.',
     'author': 'Drauthran',
     'depends': [
         'website_sale',
@@ -11,6 +11,11 @@
     'data': [
         'views/templates.xml',
     ],
+    'assets': {
+        'website.assets_frontend': [
+            'dtn_website_sale_stock_default_variant/static/src/scss/product_page_stock_style.scss',
+        ],
+    },
     'installable': True,
     'application': False,
     'auto_install': False,
